95ce5307507360bf1b45d4a8022811aa7b4d9f06
이주혜 <wngpsid4014@gmail.com>
2022-10-21 15:59:38 +0900

    copypaste. 노마드코더 css 복붙

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,22 +2,47 @@ import Link from "next/link"
 import { useRouter } from "next/router"
 
 export default function NavBar() {
-    const router = useRouter();
+  const router = useRouter();
+  return (
+    <nav>
+      <img src="/vercel.svg" />
+      <div>
+        <Link href='/'>
+            <a className={router.pathname==='/'?"active":""}>Home</a>
+        </Link>
+        <Link href='/about'>
+            <a className={router.pathname==='/about'?"active":""}>About</a>
+        </Link>
+      </div>
 
-    return (
-        <nav>
-            <Link href='/'>
-                <a className={router.pathname==='/'?"active":""}>Home</a>
-            </Link>
-            <Link href='/about'>
-                <a className={router.pathname==='/about'?"active":""}>About</a>
-            </Link>
-
-            <style jsx>{`
-                .active {
-                    color : tomato;
-                }
-            `}</style>
-        </nav>
-    )
+      <style jsx>{`
+        nav {
+          display: flex;
+          gap: 10px;
+          flex-direction: column;
+          align-items: center;
+          padding-top: 20px;
+          padding-bottom: 10px;
+          box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+            rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
+        }
+        img {
+          max-width: 100px;
+          margin-bottom: 5px;
+        }
+        nav a {
+          font-weight: 600;
+          font-size: 18px;
+        }
+        .active {
+          color: tomato;
+        }
+        nav div {
+          display: flex;
+          gap: 10px;
+        }
+      `}</style>
+      
+    </nav>
+  );
 }

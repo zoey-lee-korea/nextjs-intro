b30095318e3a1bd3bc2e6ec0b5443b9866bb5815
이주혜 <wngpsid4014@gmail.com>
2022-10-21 09:48:59 +0900

    index 화면 초기화

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-import { useState } from "react"
-
 export default function Home() {
-    const [count, setCount] = useState(0);
     return (
         <div>
             <h1>Home Page</h1>
-            <h1>number : {count}</h1>
-            <button onClick={() => setCount((x)=>(x+1)) }>+</button>    
         </div>
     ) 
 }
